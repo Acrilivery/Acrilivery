43e3684b9f5446f316c4e0b4db7f83c61bdf4bc8
BuildTools <unconfigured@null.spigotmc.org>
2024-05-22 15:58:10 +0200

    feat(website): recoded functions

--- entities/ControllerUsers.js
+++ entities/ControllerUsers.js
@@ -1,16 +1,26 @@
 const User = require("./user.js");
 
-class ControllerUsers
-{
+class ControllerUsers {
   #users = [];
-  addUser(nome, cognome, email, password, indirizzo, codiceFiscale)
-  {
-    const user = new User(nome, cognome, email, password, indirizzo, codiceFiscale);
+  addUser(nome, cognome, email, password, indirizzo, codiceFiscale) {
+    const user = new User(
+      nome,
+      cognome,
+      email,
+      password,
+      indirizzo,
+      codiceFiscale
+    );
     this.#users.push(user);
   }
-  getUsers()
-  {
+  getUsers() {
     return this.#users;
+  }
+  getUser(id) {
+    function OnFind(el) {
+      return el.getId() === id;
+    }
+    return this.#users.find(OnFind);
   }
 }
 module.export = Users;
