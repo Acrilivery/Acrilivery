8520638eee28e366756d35833151092876e4d66f
marcospeciale1 <marcospeciale16@gmail.com>
2024-05-23 08:42:36 +0200

    feat(website): add file edit file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ class App {
   #users = new ControllerUsers();
   #orders = new ControllerOrders();
   #articles = new ControllerArticles();
-  #currentUser=null;
+  #currentUser = null;
   registrati(nome, cognome, email, password, indirizzo, codiceFiscale) {
     this.#users.AddUser(
       nome,
@@ -19,30 +19,38 @@ class App {
     console.log("Registrazione avvenuta");
   }
   login(email, password) {
-    function OnFind(el)
-    {
-      if(el.getEmail() === email && el.getPassword() === password)
-        return el;
-      else
-      {
+    function OnFind(el) {
+      if (el.getEmail() === email && el.getPassword() === password) return el;
+      else {
         return null;
       }
     }
     this.#currentUser = this.#users.getUsers().find(OnFind);
-    if(this.#currentUser != null)
-      {
-        console.log("Login avvenuto correttamente.")
-      }
-      else
-      {
-        console.log("Password e/o email sbagliate.")
-      }
+    if (this.#currentUser != null) {
+      console.log("Login avvenuto correttamente.");
+    } else {
+      console.log("Password e/o email sbagliate.");
+    }
   }
-  cambioCredenziali() {}
-
-  recuperaPassword() {}
-
-  compra() {
-    
+  cambioCredenziali(nuovaPassword, nuovaEmail) {
+    this.#currentUser.setPassword(nuovaPassword);
+    this.#currentUser.setEmail(nuovaEmail);
+    if (
+      this.#currentUser.setPassword !== getPassword() &&
+      this.#currentUser.getEmail() !== getEmail()
+    ) {
+      return console.log("Credenziali cambiate con successo");
+    }
+    console.log("riprova");
   }
+
+  recuperaPassword(nuovaPassword) {
+    if (this.#currentUser.getPassword() === setPassword()) {
+      return console.log("Password recuperata con successo");
+    } else {
+      return console.log("Password sbagliata");
+    }
+  }
+
+  compra() {}
 }
